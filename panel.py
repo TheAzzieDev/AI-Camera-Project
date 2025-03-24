7aae618c5509e35778e37dbfcef822512544e4f9
TheAzzieDev <asrielrosemar@gmail.com>
2025-03-24 13:22:10 +0100

    forgot to commit all the necessary files, so ive recreated them and am currently making something more user friendly for any trying my code

--- panel.py
+++ panel.py
@@ -7,8 +7,10 @@ from datetime import datetime
 import os
 import uuid
 import glob
+import tracking
 app = Flask(__name__)
 CORS(app)
+
 
 
 @app.route("/", methods = ["GET"])
@@ -18,21 +20,24 @@ def index():
 @app.route("/panel", methods = ["GET"])
 def panel():
     path = r'static/detections/**/*.jpg'
+
     image_list = glob.glob(path, recursive = True)
+
     file_info = []
     dir_set = set()
+
+
     for image_path in image_list:
         start = image_path.find("static")
         end = start + 7
         path_name = image_path[end:]
+
         file_info.append((path_name.replace("\\", "/"), os.path.basename(image_path)))
-        dirname = os.path.basename(os.path.dirname(image_path))         
+        dirname = os.path.basename(os.path.dirname(image_path))    
         dir_set.add(dirname)
-    print(dir_set)
-    print()
-    for image_path in file_info:
-        print(image_path)
+   
     return render_template("panel.html", directories=dir_set, file_info=file_info)
+    
 
 if __name__ == "__main__":
     app.run(debug=True, host = "0.0.0.0", port=8080)
--- tracking.py
+++ tracking.py
@@ -5,49 +5,121 @@ import os
 import uuid
 import requests
 
-model = YOLO("models/yolov8n.pt")
-temp_id = None
-counter = 0
-switch = False
-
-
 def make_file(image):
+
     date = datetime.now()
-    folder_path = f"static/detections/{date.year}-{date.month}-{date.day}"
+
+    folder_path = f"static\\detections\\{date.year}-{date.month}-{date.day}"
+
     if not os.path.exists(folder_path):
-        # Create the folder/directory
+
         os.makedirs(folder_path)
+
     filename = f"{date.hour}h{date.minute}m{date.second}s_{(uuid.uuid1().hex)[0:10]}.jpg"
+
     output = os.path.join(os.getcwd(), folder_path, filename)
-    print(f"output: {output}")
+
     cv2.imwrite(output, image)
 
+
 #Enter the Ip adress for the camera in VideoCapture
-def func():
-    cap = cv2.VideoCapture("Enter ip to camerahere")
+def capture_images():
+
+    cap = cv2.VideoCapture(0)
+
+    model = YOLO("models/yolov8n.pt")
+
+    time_in_seconds = 0
+
+    time_in_seconds_before = 0
+
+
     while cap.isOpened():
+
         ret, frame = cap.read()
+
         if not ret:
             break
-        framerot = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-        result = model.track(framerot, persist=True)
-        #xyxy tracker conf clsnummer
-        #res_list = result[0].boxes.data
-        for box in result[0].boxes:
-            print(f"cls: {result[0].names[box.cls[0].item()]} confidence: {float(box.conf[0].item())}")
-            print(f"result.boxes: {result[0].boxes}")
-            cond = box.cls[0].item() == 0 
-            if cond:
-                if float(box.conf[0].item()) > 0.5:
-                    make_file(framerot)
-                    print(f"intruder: {float(box.conf[0].item())}")
+        
+        result = model.track(frame, persist=True)
+        time_in_seconds = int(datetime.now().strftime("%S"))
 
+        time_difference = abs(time_in_seconds - time_in_seconds_before)
         plotted_image = result[0].plot()
+
+        for box in result[0].boxes:
+
+
+            cond = box.cls[0].item() == 0 
+
+            if cond:
+                if float(box.conf[0].item()) > 0.5 and time_difference >= 3:
+
+                    make_file(plotted_image)
+
+                    print(f"intruder: {float(box.conf[0].item())}")
+                    time_in_seconds_before = time_in_seconds
+
+        
         cv2.namedWindow('Video', cv2.WINDOW_KEEPRATIO)
+
         # width: 1080 height: 1920
-        cv2.resizeWindow('Video', int(1080/2), int(1920/2))
+        cv2.resizeWindow('Video', int(1920/2), int(1080/2),)
+
         cv2.imshow('Video', plotted_image)
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
-       
-func()
+
+
+def capture_and_send():
+
+    cap = cv2.VideoCapture(0)
+
+    model = YOLO("models/yolov8n.pt")
+
+    time_in_seconds = 0
+
+    time_in_seconds_before = 0
+
+
+    while cap.isOpened():
+
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+        
+        result = model.track(frame, persist=True)
+        time_in_seconds = int(datetime.now().strftime("%S"))
+
+        time_difference = abs(time_in_seconds - time_in_seconds_before)
+        plotted_image = result[0].plot()
+
+        for box in result[0].boxes:
+
+
+            cond = box.cls[0].item() == 0 
+
+            if cond:
+                if float(box.conf[0].item()) > 0.5 and time_difference >= 3:
+
+                    make_file(plotted_image)
+
+                    print(f"intruder: {float(box.conf[0].item())}")
+                    time_in_seconds_before = time_in_seconds
+
+        
+        cv2.namedWindow('Video', cv2.WINDOW_KEEPRATIO)
+
+        # width: 1080 height: 1920
+        cv2.resizeWindow('Video', int(1920/2), int(1080/2),)
+
+        cv2.imshow('Video', plotted_image)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+if __name__ == "__main__":  
+
+    capture_images()
